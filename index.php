dae8992fd7593f5c2294909bc71b02ef1d184bf7
Vilija Kontrimaite <vikontrimaite@gmail.com>
2021-03-02 10:23:27 +0200

    create link to dir

--- index.php
+++ index.php
@@ -5,25 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File system browser</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
+    <h1>Hello! Welcome to the PHP File System browser</h1>
+    <table>
+        <tr>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Actions</th>
+        </tr>
 
-        $dir = scandir(".");
-        foreach ($dir as $file)
-        {
-            if (is_dir($file))
-            {
-                echo "Folder: ".$file."<br>";
-            } elseif (is_file($file)) {
-                echo "File: ".$file."<br>";
-            }
-            else
-            {
-                echo "File doesn't exist";
-            }
-        }
-    ?>
+        <?php
+            $dir = scandir('.');
+            foreach ($dir as $key) {
+                if ($key == '.' || $key == '..') {
+                    continue;
+                } elseif (is_file($key)) {
+                    echo 
+                    "<tr>
+                        <td>File</td>
+                        <td>$key</td>
+                        <td>ACTION</td>
+                    </tr>";
+                } 
+                elseif (is_dir($key)) {
+                            echo
+                            "<tr>
+                                <td>Directory</td>
+                                <td><a href=\"./$key\">$key</a></td>
+                                <td>ACTION</td>
+                            </tr>";
+                    }
+                    
+                } 
+            
+        ?>
+    </table>
 </body>
 </html>
     
